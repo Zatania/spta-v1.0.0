7fda8eb388eb540ded2b77515d8ef9f276657f25
John Binze Escol <escoljohnbinze@gmail.com>
2025-08-12 09:51:35 +0800

    Disable eslint.

--- next.config.js
+++ next.config.js
@@ -15,5 +15,10 @@ module.exports = {
     }
 
     return config
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete
+    // even if your project has ESLint errors.
+    ignoreDuringBuilds: true
   }
 }

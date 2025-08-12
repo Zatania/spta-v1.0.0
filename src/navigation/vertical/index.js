3f546c7e5b7619853954d4b32baf23ee27b337f3
John Binze Escol <escoljohnbinze@gmail.com>
2025-08-12 11:20:46 +0800

    update navigation

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -36,41 +36,6 @@ const navigation = () => {
           subject: 'attendance-page'
         }
       ]
-    },
-    {
-      title: 'User',
-      icon: 'mdi:account-outline',
-      children: [
-        {
-          title: 'List',
-          path: '/apps/user/list'
-        },
-        {
-          title: 'View',
-          children: [
-            {
-              title: 'Overview',
-              path: '/apps/user/view/overview'
-            },
-            {
-              title: 'Security',
-              path: '/apps/user/view/security'
-            },
-            {
-              title: 'Billing & Plans',
-              path: '/apps/user/view/billing-plan'
-            },
-            {
-              title: 'Notifications',
-              path: '/apps/user/view/notification'
-            },
-            {
-              title: 'Connection',
-              path: '/apps/user/view/connection'
-            }
-          ]
-        }
-      ]
     }
   ]
 }
